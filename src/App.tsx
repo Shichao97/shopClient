0ca8a47c0ae781b4914ff9b54a08c44d1088f4d5
Lee <12345@qq.com>
2020-06-17 15:24:55 +0800

    lyc2

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
 
-  
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Product = () => (
+  <div>
+    <h2>Product</h2>
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Product">Product</Link>
+          <hr/>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/product" component={Product}></Route>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import Register from './Register';
 import EditInfo from './EditInfo';
 import AddGoods from './AddGoods';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AddGoods />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -17,4 +17,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
