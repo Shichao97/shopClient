affa90a13f0e401252cc9f6a6e536b7d48341693
刘适超 <liushichao@Tracys-MacBook-Pro.local>
2020-06-22 12:10:49 -0400

    lalaho

---
+++ src/ReleaseGoods.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import LoginModal from './LoginModal';
+//import './Register.css';
+import jquery from "jquery";
+const $ = jquery;
+
+export default class ReleaseGoods extends React.Component<any,any> {
+    constructor(props:any){
+        super(props);
+        this.state={
+        }
+    }
+
+    render(){
+        return(
+            <div>
+
+
+                <img />
+               
+            </div>
+        )
+    }
+}
