414eb67b88f1ae86222311d27e47591d5fcdf600
Lee <12345@qq.com>
2020-06-23 21:01:34 +0800

    imgupload

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -4,7 +4,11 @@ import jquery from "jquery";
 const $ = jquery;
 
 export default class ImageUpload extends React.Component {
-    
+    constructor(props,state){
+        super(props,state);
+        this.state = {imgs:[]};
+    }
+
     multiImagePreview(avalue) {
         var all_picW = "100px";//预览的宽度
         var all_picH = "100px";//预览的高度
@@ -13,47 +17,41 @@ export default class ImageUpload extends React.Component {
         var picViewsBox = document.getElementById("picViewsBox");
         picViewsBox.innerHTML = "";
         var fileList = upMultilImagesObj.files;
-        for (var i = 0; i < fileList.length; i++) {            
+        //var fs = this.state.imgs;
+        //fs.concat(fileList);
+        //this.setState({imgs:fs});
+        
+        for (var i = 0; i < fileList.length; i++) { 
+            this.state.imgs.push(upMultilImagesObj.files[i]);
+            /*           
             picViewsBox.innerHTML += "<div style='float:left' > <img id='img" + i + "'  /> </div>";
             var picjPreviewInfo = document.getElementById("img"+i); 
-            //if (upMultilImagesObj.files && upMultilImagesObj.files[i]) {
-                //火狐
-                picjPreviewInfo.style.display = 'block';
-                picjPreviewInfo.style.width = all_picW;
-                picjPreviewInfo.style.height = all_picH;
-                //picjPreviewInfo.src = upMultilImagesObj.files[0].getAsDataURL();
-                //火狐7+版本,用不了getAsDataURL()
-                picjPreviewInfo.src = window.URL.createObjectURL(upMultilImagesObj.files[i]);
-            /*} else {
-                //IE
-                upMultilImagesObj.select();
-                var picTagSrc = document.selection.createRange().text;
-                alert(picTagSrc);
-                var localImagId = document.getElementById("img" + i);
-                //设置初始大小
-                localImagId.style.width = all_picW;
-                localImagId.style.height = all_picH;
-                //图片异常捕捉
-                try {
-                    localImagId.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(sizingMethod=scale)";
-                    localImagId.filters.item("DXImageTransform.Microsoft.AlphaImageLoader").src = picTagSrc;
-                }
-                catch (e) {
-                    alert("图片格式不正确!");
-                    return false;
-                }
-                picjPreviewInfo.style.display = 'none';
-                document.selection.empty();
-            }*/
+            picjPreviewInfo.style.display = 'block';
+            picjPreviewInfo.style.width = all_picW;
+            picjPreviewInfo.style.height = all_picH;
+            picjPreviewInfo.src = window.URL.createObjectURL(upMultilImagesObj.files[i]);
+            */
         }  
+        this.setState({});
         return true;
     }   
 
+    imgClicked(index){
+        this.state.imgs.splice(index,1);
+        this.setState({});
+    }
     
     render(){
     return <div>
 	    <input type="file"  name="file" id="upMultilImages" multiple="multiple"   onChange={() => this.multiImagePreview()} accept="image/*" />
 	    <div id="picViewsBox"></div>
+        {
+            this.state.imgs.map((element,index) =>{
+                return <div className="upimgs"> 
+                <img width="100px" height="100px" onClick={()=> this.imgClicked(index)}
+                src={window.URL.createObjectURL(element)} /> </div>
+            })
+        }
     </div>  
     }
 }
