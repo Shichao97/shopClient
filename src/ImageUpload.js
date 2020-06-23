a6e0963b0620c7c9eb5c84d41691cbd0afecf3a9
Lee <12345@qq.com>
2020-06-23 21:03:53 +0800

    upimg

--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -10,30 +10,16 @@ export default class ImageUpload extends React.Component {
     }
 
     multiImagePreview(avalue) {
-        var all_picW = "100px";//预览的宽度
-        var all_picH = "100px";//预览的高度
-
+ 
         var upMultilImagesObj = document.getElementById("upMultilImages");
         var picViewsBox = document.getElementById("picViewsBox");
         picViewsBox.innerHTML = "";
         var fileList = upMultilImagesObj.files;
-        //var fs = this.state.imgs;
-        //fs.concat(fileList);
-        //this.setState({imgs:fs});
         
         for (var i = 0; i < fileList.length; i++) { 
             this.state.imgs.push(upMultilImagesObj.files[i]);
-            /*           
-            picViewsBox.innerHTML += "<div style='float:left' > <img id='img" + i + "'  /> </div>";
-            var picjPreviewInfo = document.getElementById("img"+i); 
-            picjPreviewInfo.style.display = 'block';
-            picjPreviewInfo.style.width = all_picW;
-            picjPreviewInfo.style.height = all_picH;
-            picjPreviewInfo.src = window.URL.createObjectURL(upMultilImagesObj.files[i]);
-            */
         }  
         this.setState({});
-        return true;
     }   
 
     imgClicked(index){
@@ -44,7 +30,6 @@ export default class ImageUpload extends React.Component {
     render(){
     return <div>
 	    <input type="file"  name="file" id="upMultilImages" multiple="multiple"   onChange={() => this.multiImagePreview()} accept="image/*" />
-	    <div id="picViewsBox"></div>
         {
             this.state.imgs.map((element,index) =>{
                 return <div className="upimgs"> 
@@ -52,6 +37,6 @@ export default class ImageUpload extends React.Component {
                 src={window.URL.createObjectURL(element)} /> </div>
             })
         }
-    </div>  
+        </div>  
     }
 }
