1dd9ff0f54b5c2f9acdce2e56407b0a681333085
刘适超 <liushichao@Tracys-MacBook-Pro.local>
2020-06-25 00:54:22 -0400

    editimg

--- src/EditSellGoods.tsx
+++ src/EditSellGoods.tsx
@@ -4,11 +4,49 @@ import jquery from "jquery";
 const $ = jquery;
 
 export default class EditSellGoods extends React.Component<any,any>{
+    constructor(props:any){
+        super(props);
+        this.state = {
+          imgName:[]
+        }
+      }
+
+    
+    getExistImg(gid:number){
+        let newUrl:string = window.localStorage.getItem("host_pre")+"goods/sell/getgoodsinfo?Id="+gid;
+        let _this:EditSellGoods = this;
+        $.ajax({
+            type:"GET",
+            crossDomain: true, 
+            xhrFields: {
+                withCredentials: true 
+            },
+            url:newUrl,
+            dataType:"json",
+            success:function(data){
+                let imgStr:string = data.filename;
+                let arr:string[] = imgStr.split(";");
+                _this.setState({imgName:arr});
+            }
+          })
+    }
     render(){
         let gid:number = this.props.match.params.gid;
+        this.getExistImg(gid);
+        let imgname:string[] = this.state.imgName;
+        
         return(
             <div>
-            
+                {imgname.map((element:any) =>{
+                      var myDate = new Date();
+                      let imgSrc:string = window.localStorage.getItem("host_pre")+"goods/sell/getgoodsimg?Id="+gid+"&fname="+element+"&refresh="+myDate.getMilliseconds();
+      
+                      return(
+                        <img src={imgSrc}/>
+                      )
+                    
+                    }
+                )}
             
             </div>
         )
