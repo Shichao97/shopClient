0120e8dd97cea0e5cee5ebe35a4af2cc2408b0d4
Lee <12345@qq.com>
2020-07-08 21:38:37 +0800

    dd

--- src/MessagePanel.jsx
+++ src/MessagePanel.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react';
 import Modal from 'react-modal';
-//import './App.css';
+import './MessagePanel.css';
 import { render } from '@testing-library/react';
 import jquery from "jquery";
 import { Button, Row, Col } from 'antd'
@@ -34,36 +34,134 @@ const $ = jquery;
                     })
                     
                 }
-                test hello
-                <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>        
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>
-        <li>444</li>            
+<p className="leftd">
+ 
+ <span className="leftd_h">
+
+     <img src="./img/c_pic.pn" />
+
+ </span>
+
+ <p className="speech left"> 
+
+     二货，你看你傻样！
+
+ </p>
+
+</p>
+
+<p className="rightd">
+
+ <span className="rightd_h">
+
+     <img src="./img/u_pic.pn" />
+
+ </span>
+
+ <p className="speech right"> 
+
+     嘻嘻嘻嘻。。。。。。
+
+ </p>
+
+</p>
+
+<p className="leftd">
+
+ <span className="leftd_h">
+
+     <img src="./img/c_pic.pn" />
+
+ </span>
+
+ <p className="speech left"> 
+
+     笑什么笑，没看到本宝宝今天变漂亮了吗？
+
+ </p>
+
+</p>
+
+<p className="rightd">
+
+ <span className="rightd_h">
+
+     <img src="./img/u_pic.pn" />
+
+ </span>
+
+ <p className="speech right"> 
+
+      不不不，每天你都很漂亮的啦！
+
+ </p>
+
+</p>
+
+<p className="leftd">
+ 
+ <span className="leftd_h">
+
+     <img src="./img/c_pic.pn" />
+
+ </span>
+
+ <p className="speech left"> 
+
+     二货，你看你傻样！
+
+ </p>
+
+</p>
+
+<p className="rightd">
+
+ <span className="rightd_h">
+
+     <img src="./img/u_pic.pn" />
+
+ </span>
+
+ <p className="speech right"> 
+
+     嘻嘻嘻嘻。。。。。。
+
+ </p>
+
+</p>
+
+<p className="leftd">
+
+ <span className="leftd_h">
+
+     <img src="./img/c_pic.pn" />
+
+ </span>
+
+ <p className="speech left"> 
+
+     笑什么笑，没看到本宝宝今天变漂亮了吗？
+
+ </p>
+
+</p>
+
+<p className="rightd">
+
+ <span className="rightd_h">
+
+     <img src="./img/u_pic.pn" />
+
+ </span>
+
+ <p className="speech right"> 
+
+      不不不，每天你都很漂亮的啦！
+
+ </p>
+
+</p>
+     
         </div>
         <div className='chat_center'>
             <input type="text" id="msgtext" name="test"/>
